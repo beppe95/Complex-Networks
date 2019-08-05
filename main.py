171a62777d12404df35a1fa4bb5f83008e524820
beppe95 <37592014+beppe95@users.noreply.github.com>
2019-08-05 18:48:32 +0200

    Added some utils.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from utils import get_graph_from_file
+from utils import get_graph_from_file, increase_density, get_giant_connected_component, get_eccentricity, \
+    get_clustering_values, get_centralities
 
 if __name__ == '__main__':
-    g = get_graph_from_file()
+    graph = get_graph_from_file()
+
+    '''
+    
+    gcc = get_giant_connected_component(graph)
+    print(gcc)
+    
+    ecc = get_eccentricity(graph)
+    print(ecc)
+    
+    cc_val = get_clustering_values(graph)
+    print(cc_val[0], cc_val[1], cc_val[2])
+    
+    
+    '''
--- utils.py
+++ utils.py
@@ -1,13 +1,62 @@
 from pathlib import Path
-from networkx import Graph, DiGraph, read_edgelist
+import networkx as nx
+import pandas as pd
 
-
-graph_name= 'amazon'
-graph_type = 'un'
+graph_name = 'wikivote'
+graph_type = 'di'
 datasets_path = Path.cwd() / 'datasets'
 graph_path = datasets_path / graph_name / str(graph_name + '.txt')
 
 
 def get_graph_from_file():
-    return read_edgelist(graph_path, create_using=Graph(), nodetype=int) if graph_type == 'un' \
-        else read_edgelist(graph_path, create_using=DiGraph(), nodetype=int)
+    return nx.read_edgelist(graph_path, create_using=nx.Graph(), nodetype=int) if graph_type == 'un' \
+        else nx.read_edgelist(graph_path, create_using=nx.DiGraph(), nodetype=int)
+
+
+def increase_density(graph):
+    density = nx.density(graph)
+    core = [node for node, deg in nx.degree(graph) if deg >= 2]
+    reduced_graph = nx.subgraph(graph, core)
+    new_density = nx.density(reduced_graph)
+
+    return reduced_graph if new_density > density else graph
+
+
+def get_giant_connected_component(graph):
+    gcc = [type(graph)(), "id"]
+    scc = nx.strongly_connected_component_subgraphs(graph)
+    for n_id, graph in enumerate(scc):
+        if len(graph) > len(gcc[0]):
+            gcc = [graph, "scc_" + str(n_id) + ".pickle"]
+
+    nx.write_gpickle(gcc[0], path=gcc[1])
+    return gcc
+
+
+# Non mi convince molto
+def get_eccentricity(graph):
+    try:
+        ecc = nx.eccentricity(type(graph)())
+        return ecc
+    except nx.exception.NetworkXError:  # Found infinite path length because the graph is not strongly connected
+        eccentricity = []
+        scc = nx.strongly_connected_component_subgraphs(graph)
+        for n_id, graph in enumerate(scc):
+            if len(graph) > 1:
+                ecc = nx.eccentricity(graph)
+                eccentricity.append((nx.center(scc, ecc), nx.periphery(scc, ecc),
+                                     nx.radius(scc, ecc), nx.diameter(scc, ecc)))
+
+    return eccentricity
+
+
+def get_clustering_values(gcc, ego) -> tuple:
+    clustering_coefficient = nx.clustering(gcc, ego)
+    avg_clustering_coefficient = nx.average_clustering(gcc)
+    transitivity = nx.transitivity(gcc)
+
+    return clustering_coefficient, avg_clustering_coefficient, transitivity
+
+
+def get_centralities(graph):
+    return None
